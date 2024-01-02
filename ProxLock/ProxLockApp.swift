fece46cc32b8ab36b6e7c02f43365ab3da986160
levarr <fabcolonna@icloud.com>
2024-01-03 00:38:23 +0100

    Initial Commit

---
+++ ProxLock/ProxLockApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProxLockApp.swift
+//  ProxLock
+//
+//  Created by Fabio Colonna on 03/01/24.
+//
+
+import SwiftUI
+
+@main
+struct ProxLockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
